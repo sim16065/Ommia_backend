79f8686537013e1caece70b09754f09a73321fbe
kangseungmin <ksmzaq@gmail.com>
2026-07-10 08:01:46 +0900

    fix: 애플리케이션 main 메서드 접근자를 public으로 변경

--- src/main/java/com/mia/community/CommunityApplication.java
+++ src/main/java/com/mia/community/CommunityApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CommunityApplication {
 
-     static void main(String[] args) {
+     public static void main(String[] args) {
         SpringApplication.run(CommunityApplication.class, args);
     }
 }
